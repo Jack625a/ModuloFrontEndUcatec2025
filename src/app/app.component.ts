605ead864caf08c111569c7552811a17587189ce
Jack625a <ktfut15@gmail.com>
2025-05-12 19:51:02 -0400

    CardViews Dinamicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,80 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+//Importacion del modulo
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+//Common MOdule
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatCardModule, CommonModule,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'componentes';
+
+  datos=[
+    {
+      nombre:'Producto 1',
+      descripcion:'Descripcion producto 1',
+      precio:150,
+      imagen:'https://www.tiendaamiga.com.bo/media/catalog/product/cache/deb88dadd509903c96aaa309d3e790dc/h/p/hp_15_ef2517la.jpg'
+    },
+    {
+      nombre:'Producto 2',
+      descripcion:'Descripcion producto 2',
+      precio:350,
+      imagen:'https://www.tiendaamiga.com.bo/media/catalog/product/cache/deb88dadd509903c96aaa309d3e790dc/a/s/asus_e1504fa-nj697.jpg'
+    },
+    {
+      nombre:'Producto 3',
+      descripcion:'Descripcion producto 3',
+      precio:450,
+      imagen:'https://www.tiendaamiga.com.bo/media/catalog/product/cache/deb88dadd509903c96aaa309d3e790dc/c/0/c01269.jpg'
+    },
+    {
+      nombre:'Producto 4',
+      descripcion:'Descripcion producto 4',
+      precio:1050,
+      imagen:'https://www.tiendaamiga.com.bo/media/catalog/product/cache/deb88dadd509903c96aaa309d3e790dc/h/p/hp_15_ef2517la.jpg'
+    },
+    {
+      nombre:'Producto 5',
+      descripcion:'Descripcion producto 5',
+      precio:3150,
+      imagen:'https://www.tiendaamiga.com.bo/media/catalog/product/cache/deb88dadd509903c96aaa309d3e790dc/h/p/hp_15_ef2517la.jpg'
+    },
+     {
+      nombre:'Producto 6',
+      descripcion:'Descripcion producto 6',
+      precio:3150,
+      imagen:'https://www.tiendaamiga.com.bo/media/catalog/product/cache/deb88dadd509903c96aaa309d3e790dc/h/p/hp_15_ef2517la.jpg'
+    },
+     {
+      nombre:'Producto 7',
+      descripcion:'Descripcion producto 7',
+      precio:3150,
+      imagen:'https://www.tiendaamiga.com.bo/media/catalog/product/cache/deb88dadd509903c96aaa309d3e790dc/h/p/hp_15_ef2517la.jpg'
+    },
+     {
+      nombre:'Producto 8',
+      descripcion:'Descripcion producto 8',
+      precio:3150,
+      imagen:'https://www.tiendaamiga.com.bo/media/catalog/product/cache/deb88dadd509903c96aaa309d3e790dc/h/p/hp_15_ef2517la.jpg'
+    },
+     {
+      nombre:'Producto 9',
+      descripcion:'Descripcion producto 9',
+      precio:3150,
+      imagen:'https://www.tiendaamiga.com.bo/media/catalog/product/cache/deb88dadd509903c96aaa309d3e790dc/h/p/hp_15_ef2517la.jpg'
+    },
+     {
+      nombre:'Producto 10',
+      descripcion:'Descripcion producto 10',
+      precio:3150,
+      imagen:'https://www.tiendaamiga.com.bo/media/catalog/product/cache/deb88dadd509903c96aaa309d3e790dc/h/p/hp_15_ef2517la.jpg'
+    },
+  ]
 }
