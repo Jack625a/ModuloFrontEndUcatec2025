289732e475a9585259a97e529652434871dd58f5
Jack625a <ktfut15@gmail.com>
2025-05-21 19:48:35 -0400

    firebase

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,31 +1,43 @@
-import { Component,inject, signal} from '@angular/core';
-//Importar las dependencias
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged} from "@angular/fire/auth";
-
-
+import { Auth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [CommonModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  template: `
+    <div *ngIf="!user(); else loggendIn">
+    <h2>Iniciar Sesion con Google</h2>
+    <button (click)="login()" >Inciar Sesion</button>
+</div>
+
+<ng-template #loggendIn>
+    <h3>Hola, {{user()?.displayName}} </h3>
+    <br>
+    <img [src]="user()?.photoURL" />
+    <br>
+    <p>{{user()?.email}} </p>
+    <button (click)="logout()">Cerrar Sesion</button>
+
+</ng-template>
+  `
+
 })
 export class LoginComponent {
-  //estructura de la sesion
-  private auth=getAuth();
-  user=signal<any|null>(null);
-  constructor(){
-    onAuthStateChanged(this.auth, (u)=>this.user.set(u));
-  }
-  login(){
-    const proveedor=new GoogleAuthProvider();
-    signInWithPopup(this.auth,proveedor).catch(error =>
-      alert(error.message)
-    );
+  private auth = inject(Auth);
+  user = signal<any | null>(null);
+
+  constructor() {
+    onAuthStateChanged(this.auth, (u) => this.user.set(u));
   }
 
-  logout(){
+  login() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(this.auth, provider).catch(err => alert(err.message));
+  }
+
+  logout() {
     signOut(this.auth);
   }
 }
